977e0e000480e5e26371f18d400a2e0e6323a893
Kelsey Ruckert <klr324@client-104-39-53-140.mobility.psu.edu>
2017-01-03 13:49:19 -0500

    update license and add version history

--- Ruckert_2016_DAIS_Code/dais.c
+++ Ruckert_2016_DAIS_Code/dais.c
@@ -1,20 +1,31 @@
-// Copyright 2016 Robert W. Fuller <hydrologiccycle@gmail.com>
 //
-// This program is free software: you can redistribute it and/or modify
+// file: dais.c  21 Jun 2016; edited 04 Aug 2016
+//
+// Author: Robert Fuller   (rwf136@psu.edu)
+// Edits by Kelsey Ruckert (klr324@psu.edu)
+// Pennsylvania State University
+//
+// Note: The DAIS model as originally described in Shaffer (2014) and updated to C
+//
+// Version History:
+//   1.0 - 2014 - Initial coding in matlab (Gary Shaffer)
+//   2.0 - 23 July 2014 - Converted from matlab to R (Kelsey Ruckert; klr324@psu.edu)
+//   2.5 - 28 July 2016 - Beta equation corrected with respect to Shaffer (2014) text (Kelsey Ruckert; klr324@psu.edu)
+//   3.0 - 04 Aug 2016 - Converted from R to C (Robert Fuller; rwf136@psu.edu)
+//
+// Copyright 2016 Robert Fuller
+// This file is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// This program is distributed in the hope that it will be useful,
+// This file is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//
-// dais.c
-// written by Robert W. Fuller on 160621
+// along with this file.  If not, see <http://www.gnu.org/licenses/>.
 //
 
 #include "r.h"
