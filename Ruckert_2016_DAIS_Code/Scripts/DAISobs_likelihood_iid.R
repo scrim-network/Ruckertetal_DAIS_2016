2107a8a456c15fc5cd1709dd6f8706f1ab0efc0a
Kelsey Ruckert <klr324@client-104-39-53-140.mobility.psu.edu>
2017-01-03 15:20:21 -0500

    add copyright statement and comments
    Also deleted unused/ commented out codes and fixed typos in the
    comments. The point is to increase readability.

--- Ruckert_2016_DAIS_Code/Scripts/DAISobs_likelihood_iid.R
+++ Ruckert_2016_DAIS_Code/Scripts/DAISobs_likelihood_iid.R
@@ -1,13 +1,27 @@
 #####################################################################################
-# R Function: Daisobs_likelihood_iid.R
+# R Function: DAISobs_likelihood_iid.R
 # -Antarctic Ice Sheet (AIS) model
 #
 # compute (log) likelihood for observations
 # The observations are indepent and identically distributed (IID)
 #
-# -Author: Yawen Guan and Kelsey Ruckert (klr324@psu.edu)
-#####################################################################################
+# -Author: Yawen Guan (yig5031@psu.edu) and Kelsey Ruckert (klr324@psu.edu)
 # -June 10 2015
+##==============================================================================
+## Copyright 2015 Yawen Guan, Kelsey Ruckert
+## This file is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This file is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this file.  If not, see <http://www.gnu.org/licenses/>.
+##==============================================================================
 #####################################################################################
 
 log.lik = function(p) # model.p is the dimension of model parameters 
@@ -22,14 +36,17 @@ log.lik = function(p) # model.p is the dimension of model parameters
   
   llik.y  = 0
   
-  #get the residuals
+  # get the residuals
+  # the paleo observational constraints are relative to 1961-1990 average
   r1 = median(windows[1,]) - (y.mod[120000] - mean(y.mod[SL.1961_1990]))
   r2 = median(windows[2,]) - (y.mod[220000] - mean(y.mod[SL.1961_1990]))
   r3 = median(windows[3,]) - (y.mod[234000] - mean(y.mod[SL.1961_1990]))
   #r4 = median(windows[4,]) - (y.mod[240002] - mean(y.mod[SL.1961_1990]))
+  
+  # the 2002 observational constraint is relative to the year 1992 and not 1961-1990
   r4 = median(windows[4,]) - (y.mod[240002] - y.mod[239992])
 
-#resid.y = c(r1, r2, r3, r4)
+  #resid.y = c(r1, r2, r3, r4)
   resid.paleo = c(r1, r2, r3)
   resid.inst = r4
   sterr.y = obs.errs #This makes the model heteroskedastic
@@ -37,7 +54,6 @@ log.lik = function(p) # model.p is the dimension of model parameters
   #Calculate the likelihood. The observations are not correlated. They are independent
   llik.paleo = sum(dnorm(resid.paleo, mean=rep(0,3), sd = sqrt(var.paleo + sterr.y[1:3]^2), log=TRUE))
   llik.inst = sum(dnorm(resid.inst, mean=0, sd = sqrt(var.inst + sterr.y[4]^2), log=TRUE))
-  #llik.inst = sum(dnorm(resid.inst, mean=0, sd = sqrt(0 + sterr.y[4]^2), log=TRUE))
   
   llik = llik.paleo + llik.inst # assume residuals are independent
   llik  
@@ -48,26 +64,18 @@ log.pri = function(p)
   var.paleo = p[model.p+1]
   var.inst = p[model.p+2]
 
-# var.y has inverse gamma prior, so there is a lower bound at 0 but no upper bound
+# var.paleo has inverse gamma prior, so there is a lower bound at 0 but no upper bound
   in.range = all(p > bound.lower) & all(p < bound.upper)
   
   alpha_var = 2
   beta_var = 1
   var_pri_paleo = 0
-  
-  #alpha_var_inst = 5.5
-  #beta_var_inst = 0.25
-  #alpha_var_inst = 5600
-  #beta_var_inst = 1.8e-05
-  #alpha_var_inst = 100
-  #beta_var_inst = 0.01
   var_pri_inst = 0
   
   if(in.range) {
     var_pri_paleo = (-alpha_var - 1)*log(var.paleo) + (-beta_var/var.paleo)
-    #var_pri_inst  = (-alpha_var_inst - 1)*log(var.inst) + (-beta_var_inst/var.inst)
       
-    lpri=0 + var_pri_paleo + var_pri_inst
+    lpri = 0 + var_pri_paleo + var_pri_inst
   } else {
     lpri = -Inf
   }
