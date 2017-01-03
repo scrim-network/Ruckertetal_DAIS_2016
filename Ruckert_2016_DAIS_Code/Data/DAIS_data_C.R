7a50059715daeb5320f840df431a75804723cf9e
Kelsey Ruckert <klr324@client-104-39-53-140.mobility.psu.edu>
2017-01-03 15:11:31 -0500

    add copyright

--- Ruckert_2016_DAIS_Code/Data/DAIS_data_C.R
+++ Ruckert_2016_DAIS_Code/Data/DAIS_data_C.R
@@ -13,6 +13,21 @@
 ## -Author: Kelsey Ruckert (klr324@psu.edu)
 ##############################################################
 ## -June 17, 2014 #Updates June 10 2015
+##==============================================================================
+## Copyright 2015 Kelsey Ruckert
+## This file is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This file is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this file.  If not, see <http://www.gnu.org/licenses/>.
+##==============================================================================
 ##############################################################
 # read in forcing data
 if (!exists('SL')) {
